5434ce03d397552eae788f31e6fa69d223007001
SahilImrose <arkhasan16@gmail.com>
2022-02-09 22:19:19 +0600

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'client-wor',
+    appId: '1:1058302673635:web:6c3abdf4fc6a8f34bb7bd6',
+    storageBucket: 'client-wor.appspot.com',
+    apiKey: 'AIzaSyCEXx3meMMr-vA2yaGd2Ra5pxtoUD9nnpM',
+    authDomain: 'client-wor.firebaseapp.com',
+    messagingSenderId: '1058302673635',
+  },
+  production: true
+};
